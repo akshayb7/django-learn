7e7fb18fe9bdb31747257b31899a23e36084d25b
Akshay Bhardwaj <akshay10.bhardwaj@gmail.com>
2024-09-15 20:23:45 +0530

    ADD: few sample posts

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {"id": 1, "title": "First Post", "content": "This is the first post"},
+    {"id": 2, "title": "Second Post", "content": "This is the second post"},
+    {"id": 3, "title": "Third Post", "content": "This is the third post"},
+]
+
 
 # Create your views here.
-def helloWorld(request):
-    return HttpResponse("Hello World!")
+def home(request):
+    html = ""
+    for post in posts:
+        html += f"""
+        <div>
+            <h1>{post['id']} - {post['title']}</h1>
+            <p>{post['content']}</p>
+        </div>
+        """
+    return HttpResponse(html)
